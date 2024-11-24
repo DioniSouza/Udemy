fe8f227780e99d13d060e1463ac4eb15da2d6c1b
Dioni Souza <dioni.s@hotmail.com>
2024-11-24 12:19:04 -0300

    Inicio do curso de python Udemy, reparendendo o basico X-D

---
+++ aula8.py
@@ -0,0 +1,13 @@
+nome = 'Dioni'
+sobrenome = 'Souza'
+idade = 35
+ano_nascimento = 2024 - idade
+maior_idade = idade >= 18
+altura_metros = 1.83
+
+print('Nome: ', nome)
+print('Sobrenome: ', sobrenome)
+print('Idade: ', idade)
+print('Ano de nascimento: ', ano_nascimento)
+print('É maior de idade?', maior_idade)
+print('Altura em metros: ', altura_metros)
